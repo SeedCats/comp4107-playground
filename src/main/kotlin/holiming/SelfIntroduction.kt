7c6ebe1f6bd330044d6b4b88677782c69972ea6f
SeedCats <s928641007@gmail.com>
2025-03-05 15:53:44 +0800

    Add self-introduction feature

---
+++ src/main/kotlin/holiming/SelfIntroduction.kt
@@ -0,0 +1,10 @@
+package hk.edu.hkbu.comp.comp4107.playground.holiming
+
+class SelfIntroduction {
+    fun selfIntroduction() {
+        println("Hello, my name is Ho Li Ming.")
+        println("I am a student at HKBU.")
+        println("I am studying Computer Science.")
+        println("I am interested in machine learning.")
+    }
+}
